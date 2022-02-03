f0e336cbd331d88600257c4eb7f43502cc647af3
Alejandro Torancio <alejandro.torancio@extrados.com.ar>
2022-02-03 12:54:39 -0300

    Fix a swagger

--- ApiProyectosGitHub/Program.cs
+++ ApiProyectosGitHub/Program.cs
@@ -12,8 +12,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+    //c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    //c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
 });
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(options =>
